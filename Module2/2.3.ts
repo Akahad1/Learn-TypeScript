36b969d40ef38fbef4f1e8ee5dd21afaad1a4e39
Akahad1 <aksahad234567@gmail.com>
2024-05-03 12:16:06 +0600

    2.3

---
+++ Module2/2.3.ts
@@ -0,0 +1,21 @@
+{
+
+type Generic<T> =Array<T>
+
+const user:Generic<string> =['satah','rafi','mafi']
+const user2:Generic<number> =[1,22,223]
+const user3:Generic<boolean> =[true,false,true]
+
+ 
+type GenericTouple<x,y> =[x,y]
+
+const user4:GenericTouple<string,string> =['sahad','sahad']
+
+
+
+
+
+
+
+
+}
